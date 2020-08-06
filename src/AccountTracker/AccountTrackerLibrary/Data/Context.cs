d37b1a55d440bbb67f7605d6dcd8f2392cd3f5d9
Mark Taylor <Mark.Taylor737@gmail.com>
2020-08-06 18:17:21 -0400

    Created DB context class, modified Transaction.Amount property precision, and set DBInitializer to inherit from dropmodelifchanges. DBContext linked to said DB initializer through the connfig file .

---
+++ src/AccountTracker/AccountTrackerLibrary/Data/Context.cs
@@ -0,0 +1,43 @@
+﻿using AccountTrackerLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTrackerLibrary.Data
+{
+    /// <summary>
+    /// Entity Framework context class.
+    /// </summary>
+    public class Context : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<TransType> TransTypes { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+
+
+        //DBInitializer for this context class is set in the config file. As no further constructor specifications required, no consructor is present.        
+            
+        /// <summary>
+        /// Adjusting model build conventions to fit application needs.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Removing the pluralizing table name convention 
+            // so our table names will use our entity class singular names.
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            // Using the fluent API to configure the precision and scale
+            // for Transactions.Amount.
+            modelBuilder.Entity<Transaction>()
+                .Property(t => t.Amount)
+                .HasPrecision(16, 2);
+        }
+    }
+}
---
+++ src/AccountTracker/AccountTrackerLibrary/Data/DatabaseInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTrackerLibrary.Data
+{
+    /// <summary>
+    /// Custom database initializer class used to populate the database with seed data.
+    /// </summary>
+    class DatabaseInitializer : DropCreateDatabaseIfModelChanges<Context>
+    {
+        //Insert seed data for the database here.
+
+
+    }
+}
